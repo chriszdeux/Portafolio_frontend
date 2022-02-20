b03f66a28a433d042e4dc8eb144a12aaebe7439f
chriszdeux <chriszdeux@gmail.com>
2022-02-20 12:49:57 -0600

    background skills done

--- src/components/main-page/Skills.jsx
+++ src/components/main-page/Skills.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-const image = 'https://firebasestorage.googleapis.com/v0/b/proyects-screenshots.appspot.com/o/portafolio%2Fundraw_certificate_re_yadi.svg?alt=media&token=64cf4067-bbf8-46d5-a667-7fadb18ebc1b'
+const image =
+  "https://firebasestorage.googleapis.com/v0/b/proyects-screenshots.appspot.com/o/portafolio%2Fskills.jpg?alt=media&token=70fc8c5b-543c-4844-acd6-49d65d732975";
 export const Skills = () => {
   return (
-    <div  className="main__sections skills__area animation__animated animation__fadeIn" style={{ animationDelay: '1s' }}>
-      <NavLink exact to='/skills'>
-      <h2 className="titles"> Skills </h2>
+    <div
+      className="main__sections skills__area animation__animated animation__fadeIn"
+      style={{ animationDelay: "1s" }}
+    >
+      <NavLink exact to="/skills">
+        <h2 className="titles"> Skills </h2>
       </NavLink>
-      {/* <figure className='section__background' style={{
-        width: '70%',
-        diplay: 'flex',
-        justifyContent: 'flex-end',
-      }}>
+      <figure className='section__background'>
         <img src={ image } alt="" />
-      </figure> */}
+      </figure>
     </div>
-  )
-}
+  );
+};
